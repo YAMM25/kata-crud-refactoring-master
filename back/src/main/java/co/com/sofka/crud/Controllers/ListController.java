b8f6ebc51bbd10f56ba809cb693e594a2b7c360b
YAMM25 <36430354+YAMM25@users.noreply.github.com>
2021-06-04 20:07:57 -0500

    Se modificaron las interface ListControler y ListTodo

--- back/src/main/java/co/com/sofka/crud/Controllers/ListController.java
+++ back/src/main/java/co/com/sofka/crud/Controllers/ListController.java
@@ -1,4 +1,42 @@
 package co.com.sofka.crud.controllers;
 
+import co.com.sofka.crud.entity.MayorList;
+import co.com.sofka.crud.service.MayorListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 public interface ListController {
+    @Autowired
+    private MayorListService serviceMayorList;
+
+    @GetMapping(value = "api/mayorlists")
+    public Iterable<MayorList> list(){
+        return serviceMayorList.list();
+    }
+
+    @PostMapping(value = "api/mayorlist")
+    public MayorList save(@RequestBody MayorList mayorList){
+        return serviceMayorList.save(mayorList);
+    }
+
+    @PutMapping(value = "api/mayorlist")
+    public MayorList update(@RequestBody MayorList mayorList){
+        if(mayorList.getId() != null){
+            return serviceMayorList.save(mayorList);
+        }
+        throw new RuntimeException("No existe el id para actualziar");
+    }
+
+    @DeleteMapping(value = "api/{id}/mayorlist")
+    public void delete(@PathVariable("id")Long id){
+        serviceMayorList.delete(id);
+    }
+
+    @GetMapping(value = "api/{id}/mayorlist")
+    public MayorList get(@PathVariable("id") Long id){
+        return serviceMayorList.get(id);
+    }
 }
