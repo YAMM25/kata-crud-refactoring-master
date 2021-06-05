3cb28dc36fa2d2049c83f49063475392839c908b
YAMM25 <36430354+YAMM25@users.noreply.github.com>
2021-06-04 20:34:58 -0500

    Se Modificaron las Interfaces ListRepository y ListTodo

--- back/src/main/java/co/com/sofka/crud/Entity/ListTodo.java
+++ back/src/main/java/co/com/sofka/crud/Entity/ListTodo.java
@@ -1,4 +1,40 @@
 package co.com.sofka.crud.entity;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table( name = "listTodo")
 public interface ListTodo {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String groupname;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "identity")
+    //Se instancia la entidad
+    private Set<Todo> todo;
+
+    public Set<Todo> getTodo() {
+        return todo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
 }
